0d97b1d8d30cba30f42d028093a6b65e05281061
Jonas Felk <felkjonas@gmail.com>
2024-03-01 23:19:57 +0600

    feat: add supabase

--- app/(admin)/orders/_layout.tsx
+++ app/(admin)/orders/_layout.tsx
@@ -9,7 +9,10 @@ export default function OrdersLayout() {
       /> */}
       <Stack.Screen
         name='list'
-        options={{ headerShown: false }}
+        options={{
+          title: '',
+          headerShown: false,
+        }}
       />
     </Stack>
   )
---
+++ lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto'
+import * as SecureStore from 'expo-secure-store'
+import { createClient } from '@supabase/supabase-js'
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key)
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value)
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key)
+  },
+}
+
+const supabaseUrl = 'https://uulkslwolijhmckvtlqf.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1bGtzbHdvbGlqaG1ja3Z0bHFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkzMDQ2MzAsImV4cCI6MjAyNDg4MDYzMH0.E_rkz6X5WCokdRQs8fG8DQtDHcR2Tuf84egP6pgFgTA'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
