655a0925da1ff73349ac07e7e44c163f4d5c4de5
Jonas Felk <felkjonas@gmail.com>
2024-03-06 23:46:32 +0600

    feat: add supabase functions

--- supabase/functions/payment-sheet/index.ts
+++ supabase/functions/payment-sheet/index.ts
@@ -4,36 +4,45 @@
 
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
 import { stripe } from '../_utils/stripe.ts'
+import { createOrRetrieveProfile } from '../_utils/supabase.ts'
 
 console.log('Hello from Functions!')
 
-serve(async (req) => {
+serve(async (req: Request) => {
   try {
-    const { amount } = await req.json();
+    const { amount } = await req.json()
+
+    const customer = await createOrRetrieveProfile(req)
+
+    // Create an ephermeralKey so that the Stripe SDK can fetch the customer's stored payment methods.
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      { customer: customer },
+      { apiVersion: '2020-08-27' }
+    )
 
     // Create a PaymentIntent so that the SDK can charge the logged in customer.
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
       currency: 'usd',
-      // customer: customer,
-    });
+      customer: customer,
+    })
     const res = {
-      publishableKey: Deno.env.get('EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY'),
       paymentIntent: paymentIntent.client_secret,
-      // ephemeralKey: ephemeralKey.secret,
-      // customer: customer,
-    };
+      publishableKey: Deno.env.get('EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY'),
+      customer: customer,
+      ephemeralKey: ephemeralKey.secret,
+    }
     return new Response(JSON.stringify(res), {
       headers: { 'Content-Type': 'application/json' },
       status: 200,
-    });
+    })
   } catch (error) {
     return new Response(JSON.stringify(error), {
       headers: { 'Content-Type': 'application/json' },
       status: 400,
-    });
+    })
   }
-});
+})
 
 /* To invoke locally:
 
